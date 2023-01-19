08ee4121fc363a0b93cb1dfaa917162fe525becd
Thomas Grunauer <51534062+Thommyg5@users.noreply.github.com>
2023-01-19 18:03:04 -0500

    setting up

---
+++ src/main/java/com/JwA/DefectDumper/DefectDumperApplication.java
@@ -0,0 +1,13 @@
+package com.JwA.DefectDumper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefectDumperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DefectDumperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JwA/DefectDumper/DefectDumperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JwA.DefectDumper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DefectDumperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
